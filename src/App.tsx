b0ab76e06bd61189f721b5ff4753718e3e741bc3
Walter <wrkagel@uw.edu>
2022-02-10 07:57:50 -0800

    Initial creation of pages (mostly empty)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useIsAuthenticated } from '@azure/msal-react';
 import './App.css';
+import HomePage from './home-page';
+import LoginPage from './login-page';
+
+const isAuthenticated = useIsAuthenticated();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <h1>Triple Threat Vacations Technology Specialist Login</h1>
+    {isAuthenticated ? <HomePage /> : <LoginPage />}
+  </>);
 }
 
 export default App;
